0a4bfe31338e4d1afb748a42f67da588d1c28099
luisya22 <lmatosgarcia2@gmail.com>
2023-05-09 11:23:07 -0400

    feat: implements new ws server message

--- chat-app/src/components/ChatWindow.tsx
+++ chat-app/src/components/ChatWindow.tsx
@@ -2,17 +2,31 @@ import { createSignal, onMount } from "solid-js";
 import ChatBody, { ListElem, MessageType } from "./ChatBody";
 import ChatFooter from "./ChatFooter";
 import "./ChatWindow.css";
+import {usernames} from '../testdata/data.js';
+
+enum Command {
+    SUBSCRIBE = 0,
+    UNSUBSCRIBE = 1,
+    SEND_MESSAGE = 2
+}
 
 interface Message {
-  userId: string;
-  content: string;
+  command: Command,
+  channel: string,
+  content: string
+}
+
+interface Content {
+    username: string,
+    message: string
 }
 
 export default function ChatWindow() {
   //TODO: replace userId with correct one when auth done
   let socket: WebSocket;
   let containerRef: any;
-  const userId = Math.floor(Math.random() * 101);
+  const userId = Math.floor(Math.random() * 25);
+  const username = usernames[userId];
 
   const [messages, setMessages] = createSignal<Array<ListElem>>([
     {
@@ -62,55 +76,71 @@ export default function ChatWindow() {
   };
 
   const sendMessage = async (msg: string) => {
+
+    const content: Content = {
+        username: username,
+        message: msg
+    }
+    
     const message: Message = {
-      userId: userId.toString(),
-      content: msg,
-    };
+        command:  Command.SEND_MESSAGE,
+        channel: "general",
+        content: JSON.stringify(content)
+    }
 
-    const json = JSON.stringify(message);
+    const jsonData = JSON.stringify(message);
 
-    socket.send(json);
+    socket.send(jsonData);
 
     const messageData: ListElem = {
-      content: {
-        username: message.userId,
-        content: message.content,
-      },
-      type: MessageType.SEND,
-    };
+        content: {
+            username: content.username,
+            content: content.message
+        },
+        type: MessageType.SEND
+    }
 
-    console.log("Sent Message", json, messageData);
-
+    console.log("Sent Message", jsonData, messageData);
     addMessage(messageData);
-    console.log(messages());
   };
 
   onMount(async () => {
     // Create WebSocket connection.
-    socket = new WebSocket("ws://localhost:3000");
+    socket = new WebSocket(`ws://localhost:3000/chat?username=${username}`);
 
-    console.log("Here");
 
     // Connection opened
-    socket.addEventListener("open", (event) => {});
+    socket.addEventListener("open", (event) => {
+        const subscribeMessage = {
+            command: Command.SUBSCRIBE,
+            channel: "general"
+        }
+
+        const jsonData = JSON.stringify(subscribeMessage);
+
+        socket.send(jsonData);
+    });
 
     // Listen for messages
     socket.onmessage = (event) => {
-      console.log("msg: ", event.data);
-
       const message: Message = JSON.parse(event.data);
+      const content: Content = JSON.parse(message.content);
 
-      console.log("Received message", message);
+      if(content.username == username){
+          return
+      }
 
       const messageData: ListElem = {
-        content: {
-          username: message.userId,
-          content: message.content,
-        },
-        type: MessageType.RECEIVE,
-      };
+          content: {
+              username: content?.username!,
+              content: content?.message!
+          },
+          type: MessageType.RECEIVE
+      }
+
+      console.log("Message Received", messageData)
+
       addMessage(messageData);
-      console.log("Hello: ", messages());
     };
   });
 
