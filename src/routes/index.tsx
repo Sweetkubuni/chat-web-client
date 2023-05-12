9c5df7fc0b93769d5e0a33c2ff4b2de0367f93d2
justin <justinsweng@gmail.com>
2023-05-12 01:33:19 -0400

    move things around

---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { Title } from "solid-start";
+import ChatWindow from "~/components/ChatWindow";
+import Counter from "~/components/Counter";
+
+export default function Home() {
+  return (
+    <main>
+        <ChatWindow/>
+    </main>
+  );
+}
